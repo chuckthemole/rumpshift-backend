b7446bb98af89d8f937ec2af7123df506f332925
Chuck Thomas <chuckthemole@gmail.com>
2025-09-26 14:06:15 -0700

    Update db settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -159,16 +159,30 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# ----------------------------
+# Database configuration
+# ----------------------------
+if ENV == "prod":
+    # production database
+    DATABASES = {
+        "default": {
+            # e.g., 'django.db.backends.postgresql' or 'django.db.backends.mysql'
+            "ENGINE": os.getenv("DB_ENGINE"),
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT"),  # PostgreSQL: 5432, MySQL: 3306
+        }
     }
-}
+else:
+    # Local dev database
+    DATABASES = {
+        "default": {
+            "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.sqlite3"),
+            "NAME": BASE_DIR / os.getenv("DB_NAME", "db.sqlite3"),
+        }
+    }
 
 
 # Password validation
